918b0ada4142f65771eac9ad9b39f8ba017d65fe
Kana Mbakop Leonce Elsa <leonceelsa47@gmail.com>
2026-04-21 15:22:24 +0200

    User Service, JWT, role-based authentiation done

--- services/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class UserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
